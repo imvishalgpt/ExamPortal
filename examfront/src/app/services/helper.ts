4de1b21438458f962f81581d0be1cf9f6afea9bf
Vishal Gupta <vishalgpt296@gmail.com>
2026-03-20 00:37:27 +0530

    Fix API base URL: change port 8080 to 8081 for backend connection

--- examfront/src/app/services/helper.ts
+++ examfront/src/app/services/helper.ts
@@ -1,5 +1,5 @@
 // let baseUrl="http://localhost:8080";
 
 // let baseUrl="http://54.252.19.142:8080";
-let baseUrl="http://3.25.244.220:8080";
+let baseUrl="http://3.25.244.220:8081";
 export default baseUrl;
